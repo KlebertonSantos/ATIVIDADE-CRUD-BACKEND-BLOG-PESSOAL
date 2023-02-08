3c95047657a221f476c77b2938a0f92f01215611
klebertonSantos <kleberton.ferreira@mercadolivre.com>
2023-02-08 16:15:12 -0300

    ativiadade security server controler basic usuario

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -68,4 +68,15 @@ public class Postagem {
     public void setTema(Tema tema) {
         this.tema = tema;
     }
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
